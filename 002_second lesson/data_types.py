b2dc1d55fe5bf8d94157f2b53e6c52684e30160f
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-19 18:02:23 +0300

    added part of data types lesson

---
+++ 002_second lesson/data_types.py
@@ -0,0 +1,82 @@
+#data types
+
+#variables
+
+
+"Ariel"
+
+#name = "yakov"
+#hello = "My name is"
+
+#print(hello + name)
+
+
+##########
+# sequence
+##########
+
+#### STRINGS ####
+"Ariel"
+'Ariel'
+
+#print('Hello\n\n\n\n\nWorld')
+
+#"abcd" + "efgh"
+
+# = "abcdefgh"
+
+
+my_string = "hello world i like ice cream"
+#            0123456  .....             27
+# INDEX
+# We are counting the index from 0
+
+# my_string[N]
+
+# if i want to print "d"
+#print(my_string[10])
+
+
+
+# my_string[N:M]
+
+#print(my_string[0:5])
+#print(my_string[:5])
+
+
+#print(my_string[5:8])
+
+
+#print(my_string[10:28])
+#print(my_string[10:])
+
+#print(my_string[:])
+
+
+
+
+# my_string[N:M:X]
+
+print(my_string[6:28:2])
+
+# how can we get the same result with less code?
+print(my_string[6::2])
+
+
+#########################
+# raz question
+#example = "hello\nworld"
+#          0123456789...
+
+#print(example[0:5])
+#print(example[0:6])
+#print(example[0:7])
+
+#########################
+
+
+
+
+
+
+# non-sequence
