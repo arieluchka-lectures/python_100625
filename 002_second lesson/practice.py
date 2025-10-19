c5721170d98dfb510ba984a4e7a93a3b6d46550c
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-19 17:45:20 +0300

    added practice

---
+++ 002_second lesson/practice.py
@@ -0,0 +1,19 @@
+big_word = "helloworldiloveicecream"
+
+# What is the letter on index 5? print the letter.
+
+# what is the index of the letter "w"? print the letter (using the index)
+
+# print all the letters from index 10 to index 15 (not including)
+
+# print from the start of the string, to index 5 (not including)
+
+# print from the start of the string, to index 15 (including the letter on index 15)
+
+# print the right index ranges to get "helloworld"
+
+# print the right index ranges to get "icecream"
+
+# print starting from index 1, until index 10, every 3 chars
+
+# print all even indexes (start from beginning, till end, jumps of 2)
