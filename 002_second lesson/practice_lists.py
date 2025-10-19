4a1921d3f8ecf6b74db346087663521bc4399895
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-19 19:43:28 +0300

    added lists practice

---
+++ 002_second lesson/practice_lists.py
@@ -0,0 +1,15 @@
+pantry_items = ["flour", "sugar", "salt", "pepper", "olive oil", "vinegar"]
+
+# Print the second item in the list.
+
+# Print the second to last item in the list.
+
+# Create and print a new list containing "salt" and "pepper".
+
+# Print the first 3 items in the list.
+
+# Print every other item in the list, starting from the second item ("sugar"). כל איבר שני
+
+# Access and print the first letter of the last item in the list.
+
+# Create a new string by joining the 2nd and 3rd items from the list, but with the 3rd item reversed.
