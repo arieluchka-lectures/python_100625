16cb54735e36974783116827091333c868a42a9d
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-19 19:17:42 +0300

    added functions/methods/lists to data_types

--- 002_second lesson/data_types.py
+++ 002_second lesson/data_types.py
@@ -38,7 +38,7 @@ my_string = "hello world i like ice cream"
 
 
 
-# my_string[N:M]
+# my_string[N:M] (NOT INCLUDING M!)
 
 #print(my_string[0:5])
 #print(my_string[:5])
@@ -47,7 +47,8 @@ my_string = "hello world i like ice cream"
 #print(my_string[5:8])
 
 
-#print(my_string[10:28])
+#print(my_string[10:28]) # -> "m" is on index 27, so we need to print until 28 (not including 28)
+
 #print(my_string[10:])
 
 #print(my_string[:])
@@ -57,14 +58,43 @@ my_string = "hello world i like ice cream"
 
 # my_string[N:M:X]
 
-print(my_string[6:28:2])
+#print(my_string[6:28:2])
 
 # how can we get the same result with less code?
-print(my_string[6::2])
+#print(my_string[6::2])
+
+#print(my_string[3:16:3])
+
+#print the whole string, but only every 3 characters
+#print(my_string[::3])
+
+
+
+# COUNT FROM THE END
+#print(my_string[-1])
+
+#print(my_string[-5])
+
+
+#e=-7
+#print(my_string[6:-6])
+#print(my_string[6:-7])
+
+
+#print(my_string[::-1])
+
+#print(my_string[10:5:-1])
+
+
+
+
 
 
 #########################
 # raz question
+# how ranges will behave, if we use special characters?
+#
+#
 #example = "hello\nworld"
 #          0123456789...
 
@@ -79,4 +109,212 @@ print(my_string[6::2])
 
 
 
+
+
+
+
+
+
+
+
+
+#### Lists ####
+
+
+my_list = ["hello", "ariel", 8, 12, "banana", "israel"]
+#            0         1     2
+
+#print(my_list)
+
+#print(my_list[0])
+#print(my_list[2])
+#print(my_list[-1])
+#print(my_list[0:2])
+#print(my_list[0:5:2])
+
+
+# we want to display "hello" backwards.
+
+# word = my_list[0]
+
+# print(word[::-1])
+
+
+print(my_list[0][::-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### tuples ####
+
+#### dict (dictionary) ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################
+# functions and methods
+###################################
+
+# FUNCTIONS
+
+# print("hello world")
+
+
+#
+# def print():
+#     ...
+
+# print("ariel")
+#
+# def ariels_name(given_name):
+#     print("1 + 1 = ")
+#     print(1+1)
+#
+#     print("my name is")
+#     print(given_name)
+
+
+# ariels_name(given_name="ariel")
+# ariels_name(given_name="shahar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# city_name = "Rehovot"
+#
+# the_len_of_the_city = len(city_name)
+#
+# print(the_len_of_the_city + 5)
+# print(len(city_name))
+#
+# print(len("hello class!                   "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# METHODS
+
+
+# name = "ariel".upper()
+#
+# ["1", "2", "3"].
+
+# print(name)
+# print
+
+
+
+
+
 # non-sequence
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
