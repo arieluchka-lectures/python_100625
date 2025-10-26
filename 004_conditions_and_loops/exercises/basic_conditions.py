1eee794c5ec8af02982ca7486f5bb7b270a0ded5
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-26 20:42:13 +0200

    added 10 exercises about conditions

--- 004_conditions_and_loops/exercises/basic_conditions.py
+++ 004_conditions_and_loops/exercises/basic_conditions.py
@@ -31,3 +31,64 @@ password = "1234"
 
 #username = "user"
 #password = "1234"
+
+
+# Exercise 6: Multiple conditions with 'or'
+day = "Saturday"
+# Write an if statement that prints "It's the weekend!" if day is "Saturday" OR "Friday"
+
+
+
+# Exercise 7: Checking if item in list
+fruits = ["apple", "banana", "orange"]
+# Write an if statement that prints "We have bananas!" if "banana" is in the fruits list
+
+
+# Exercise 8: String contains substring
+sentence = "I love programming in Python"
+# Write an if statement that prints "Python mentioned!" if the word "Python" is in the sentence
+
+# Exercise 9: Negative numbers
+number = -5
+# Write an if-else statement that prints "Negative" if number is less than 0, otherwise print "Positive or zero"
+
+
+# Exercise 10: Even or odd
+num = 17
+# Write an if-else statement that prints "Even" if num is divisible by 2 (use % operator), otherwise print "Odd"
+
+
+# Exercise 11: String length check
+name = "Alexander"
+# Write an if statement that prints "Long name" if the length of name is more than 7 characters
+
+
+# Exercise 12: Comparing two numbers
+x = 10
+y = 20
+# Write a conditional statement that prints:
+# - "x is greater" if x > y
+# - "y is greater" if y > x
+# - "They are equal" if x == y
+
+
+# Exercise 13: Nested if statements
+is_raining = True
+has_umbrella = False
+# Write nested if statements:
+# - If it's raining, check if has_umbrella is True
+#   - If True, print "You can go out safely"
+#   - If False, print "You'll get wet"
+# - If it's not raining, print "Enjoy your day"
+
+
+# Exercise 14: Empty list check
+shopping_cart = []
+# Write an if-else statement that prints "Cart is empty" if shopping_cart has no items, otherwise print "You have items in cart"
+
+
+# Exercise 15: Price discount
+price = 150
+# Write an if-else statement:
+# - If price is more than 100, calculate 10% discount and print the new price
+# - Otherwise, print the original price
