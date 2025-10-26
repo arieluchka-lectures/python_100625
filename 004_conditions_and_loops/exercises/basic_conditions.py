72c8f3998f37de1a6cf06762c5bb0e12109d2c27
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-26 18:28:26 +0200

    added first exercise for conditions

---
+++ 004_conditions_and_loops/exercises/basic_conditions.py
@@ -0,0 +1,13 @@
+
+
+# Exercise 3: Checking list length
+my_list = [1, 2, 3, 4, 5]
+# Write an if statement that prints "List has more than 3 items" if the length of my_list is greater than 3
+# if it isnt greater than 3, print "The list doesnt have more than 3 items"
+
+
+
+
+
+
+
