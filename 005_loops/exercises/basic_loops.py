f46408344fb755ca80098a643acaf46c061005af
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-10-29 08:06:16 +0200

    added basic loops 2

--- 005_loops/exercises/basic_loops.py
+++ 005_loops/exercises/basic_loops.py
@@ -5,24 +5,17 @@
 # --- Exercise 1 ---
 # Given the list of numbers, use a for loop to print each number.
 # numbers = [1, 5, 8, 12, 15]
-#
-# for number in numbers:
-#     print(number)
+
 
 # --- Exercise 2 ---
 # Given the string, use a for loop to print each character on a new line.
 # my_string = "Python"
-#
-# for char in my_string:
-#     print(char)
+
 
 # --- Exercise 3 ---
 # Given the list of words, use a for loop to print the length of each word.
 # words = ["apple", "banana", "cherry"]
-#
-# for item in words:
-#     item_len = len(item)
-#     print(item_len)
+
 
 # --- Exercise 4 ---
 # Given the list of numbers, use a for loop to calculate and print their sum.
@@ -30,11 +23,10 @@
 data = [10.5, 20.0, 5.5]
 
 
-# --- Exercise 6 --- ********************************************* (how many ways we have?)
+# --- Exercise 6 ---
 # Given the list of numbers, use a for loop and an if statement
 # to print only the even numbers.
 numbers_to_check = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 
 
 # --- Exercise 7 ---
@@ -59,33 +51,10 @@ original_numbers = [1, 2, 3, 4, 5]
 # You will need a counter variable.
 count = 1
 
-# while 0 < count <= 11:
-# while count <= 11:
-# while count != 11:
-#     print(count)
-#     count = count + 1
-
-#
-# 1) if the variable/condition doesnt change
-# 2) if we start from a higher count
-# 3) count is incrementing incorrectly
 
 # --- Exercise 10 ---
 # Use a while loop to calculate the sum of numbers from 1 to 50.
 # Print the final sum.
-
-# sum = 0
-# num = 1
-#
-# while num < 501:
-#     sum += num
-#     # sum = sum + num
-#     num += 1
-#     # num = num + 1
-#     print("sum is", sum, "and next num is", num)
-#
-#
-# print("The final sum is ", sum)
 
 
 
@@ -93,83 +62,22 @@ count = 1
 # Create a simple countdown from 10 to 1 using a while loop.
 # Print each number, and then print "Blast off!" at the end.
 
-num = 10
-
-while num > 0:
-    print(num)
-    num = num - 1
 
 
-if num == 0:
-    print("Blast off!")
-# if this code is assuming that the loop went successful
-print("end of loop")
-
-
-
-
-# --- Exercise 13 ---
+# --- Exercise 12 ---
 # Find the first number greater than 100 that is divisible by 17.
 # Use a while loop to check numbers starting from 101.
 # Print the number once you find it and stop the loop.
 
 
-# --- Exercise 15 ---
+# --- Exercise 13 ---
 # Write a while loop that runs as long as a variable `power` is less than 1000.
 # In each iteration, double the value of `power` (starting from 2) and print it.
 power = 2
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# 1 - 2
-#
-#
-# 1 - 3
-#
-# one_to_three = 1 + 2 + 3
-#
-# 1 - 4
-#
-# one_to_four = one_to_three + 4
-#
-# 1 - 5
-#
-# one_to_four + 5
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# --- Exercise 14 ---
+# Given the list of numbers, use a while loop to remove and print elements
+# from the end of the list until it's empty. Use the `pop()` method.
+items_to_process = [10, 20, 30, 40]
 
---
+++ 005_loops/exercises/basic_loops_2.py
@@ -0,0 +1,48 @@
+# --- Exercise 1 ---
+# Loop through the `food_items` list. If you find "Broccoli", print "Found it!"
+# and immediately stop the loop using the `break` statement.
+food_items = ["Pizza", "Burger", "Salad", "Broccoli", "Ice Cream"]
+
+
+# --- Exercise 2 ---
+# Loop through the `mixed_numbers` list.
+# If a number is negative, use the `continue` statement to skip to the next iteration.
+# Print only the positive numbers.
+mixed_numbers = [1, 5, -3, 8, -1, 10, 4]
+
+
+# --- Exercise 3 ---
+numbers_range = range(1, 21)
+# Loop through numbers 1 to 20
+# Skip (continue) numbers divisible by 3, print all others
+
+
+# --- Exercise 4 ---
+counter = 1
+# Use a while loop to print numbers starting from 1
+# Break the loop when the counter reaches 100
+
+
+# --- Exercise 5 ---
+sentence = "Hello World Python Programming"
+# Loop through each character in the sentence
+# Skip (continue) spaces, print all other characters
+
+
+# --- Exercise 6 ---
+text = "Programming"
+# Use a loop to print the string in reverse
+# איך ניגשים לכל אות בנפרד?
+
+
+# --- Exercise 7 ---
+scores = [45, 67, 23, 89, 56, 91, 34]
+# Use a loop to find and print the maximum score
+
+
+# --- Exercise 8 ---
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Loop through the numbers
+# Skip (continue) odd numbers
+# Print even numbers
+# Break when you reach a number greater than 10
