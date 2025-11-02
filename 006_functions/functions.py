ef81aeecdfd1f936aaaa02f99f932c43d09391a7
Ariel Agranovich <ariel.limudey.hutz@gmail.com>
2025-11-02 21:48:11 +0200

    finished basic explanation about functions

---
+++ 006_functions/functions.py
@@ -0,0 +1,125 @@
+print("start of file")
+# def new_func(a, b, c):
+#     ... # BLACK BOX
+#     return result
+#
+#
+# def calculate_area_of_triangle(height, base):
+#     result = (base * height) / 2
+#     return result
+#
+#
+# triangle1 = calculate_area_of_triangle(4, 10)
+# print(triangle1)
+# triangle2 = calculate_area_of_triangle(5, 2)
+# print(triangle2)
+
+# functions are a way to create reusable code.
+# DRY (Dont Repeat Yourself)
+
+
+
+#
+# def happy_birthday_song(a, b, c):
+#     something = 10
+#     print("היום יום הולדת")
+#     print("היום יום הולדת")
+#     print("היום יום הולדת")
+
+# def func_name(parameter, parameter, parameter):
+#   ...
+
+
+
+
+
+
+# print(something) WE CANT PRINT
+
+
+# some built in functions
+# print()
+# input()
+# len()
+# range()
+# int()
+# str()
+
+# print(5, 1, "hello world", 3.14, sep="-")
+# len([1, 2, 3, 4, 5, 1, 1, 1 ,1])
+
+# happy_birthday_song()
+# print("לאריאל")
+# happy_birthday_song()
+# print("לרז")
+# happy_birthday_song()
+# print("לניר")
+
+
+
+
+
+
+#
+#
+# print("היום יום הולדת")
+# print("היום יום הולדת")
+# print("היום יום הולדת")
+# print("לניר")
+#
+# print("היום יום הולדת")
+# print("היום יום הולדת")
+# print("היום יום הולדת")
+# print("לרז")
+
+
+# 5 + "asd"
+
+def greet(name, age):
+    print(f"hi {name}! you are {age} years old!")
+
+def calculate_triangle_area(height: int, base: int): # height and base are parameters
+    result = (base * height) / 2
+    print(result)
+
+
+def calculate_bmi_of_person(weight, height):
+    ...
+
+# calculate_triangle_area(5, 10)
+# # calculate_triangle_area(6, 12)
+# # calculate_triangle_area(11, 11)
+# calculate_triangle_area("yosi", [1, 2, 3])
+# calculate_triangle_area()
+
+
+greet(900, "ariel")
+greet(age=900, name="ariel")
+# greet(name="ariel", 900) # WILL NOT WORK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("end of file")
